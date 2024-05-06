c91b54fb0324c3c2c0ae9fc930ee831b149818a8
Arrttyy <ironman60@live.com.mx>
2024-05-06 13:06:05 -0600

    first commit

---
+++ src/main/java/com/example/tablas/Table/Tables.java
@@ -0,0 +1,25 @@
+package com.example.tablas.Table;
+
+import com.example.tablas.TableInterface.ITable;
+
+public class Tables implements ITable{
+
+
+
+    @Override
+    public void showMenu() {
+        
+    }
+
+    @Override
+    public void readOption() {
+        
+    
+    }
+
+    @Override
+    public void table() {
+        
+    }
+    
+}
---
+++ src/main/java/com/example/tablas/TableInterface/ITable.java
@@ -0,0 +1,9 @@
+package com.example.tablas.TableInterface;
+
+public interface ITable {
+    void showMenu();
+
+    void readOption();
+
+    void table();
+}
