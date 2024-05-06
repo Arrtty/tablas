d12e6b1bc66716c7be411fb3f9cd505dae79e823
Arrttyy <ironman60@live.com.mx>
2024-05-06 13:22:51 -0600

    Metodo main

--- src/main/java/com/example/tablas/TablasApplication.java
+++ src/main/java/com/example/tablas/TablasApplication.java
@@ -2,13 +2,16 @@ package com.example.tablas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.*;
 import com.example.tablas.Table.Tables;
 
 @SpringBootApplication
 public class TablasApplication {
 
 	public static void main(String[] args) {
+		Tables tablas = new Tables();
+
+		tablas.showMenu();
+		tablas.readOption();
 		
 		//SpringApplication.run(TablasApplication.class, args);
 	}
