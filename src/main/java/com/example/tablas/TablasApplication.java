89c70ec81e45aadff6b20cca6d51b22c8ff0738c
Arrttyy <ironman60@live.com.mx>
2024-05-06 13:06:30 -0600

    Esqueleto de aplicacion

--- src/main/java/com/example/tablas/TablasApplication.java
+++ src/main/java/com/example/tablas/TablasApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TablasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TablasApplication.class, args);
+		
+		
+		//SpringApplication.run(TablasApplication.class, args);
 	}
 
 }
